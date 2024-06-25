5755ef14a192a8ca6b82704cb56197ebb447cb18
marginfull <129785659+marginfull@users.noreply.github.com>
2024-06-25 12:46:05 +0800

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,6 +1,7 @@
-if (window.location.hostname !== "ohkey.xyz") {
+document.addEventListener("DOMContentLoaded", function() {
+    if (window.location.hostname !== "ohkey.xyz") {
         document.body.innerHTML = `
             <h1>FcukYuo</h1>
         `;
-
-}
+    }
+});
